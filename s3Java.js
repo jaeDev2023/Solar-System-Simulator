46936f1c4fd807a0038c90ea1afd271377b0664b
jaeDev2023 <jae.dev2023@gmail.com>
2023-12-30 18:07:21 -0500

    Added rough ideas for the math used in the UpdateVelocity function

--- s3Java.js
+++ s3Java.js
@@ -8,13 +8,19 @@ class AstroObject {
         
     }
     
-    UpdateVelocity (astroBodies, timeStep)
+    UpdateVelocity (astroBodies, timeStep, gravConstant)
     {
+        // Need to create a global definition for the universal constant.
         for(let body = 0; body < astroBodies.length; body++)
         {
             if(astroBodies[body] != this)
             {
-                //let squareDistance = (this.position.subtract(body.position));
+                const squareDistance = (this.position.subtract(astroBodies[body].position));
+                const forceDirection = (astroBodies[body].position.subtract(this.position)).normalize(); // you may to use new syntax for these.
+                const force = forceDirection * gravConstant * this.mass * astroBodies[body].mass / squareDistance;
+                const acceleration = force / this.mass;
+                this.Velocity += acceleration * timeStep;
+                
             }
         }
         //console.log("--------");
@@ -23,6 +29,10 @@ class AstroObject {
     updatePostion (timeStep)
     {
         this.position += currentVelocity * timeStep;
+    }
+
+    printDebugging ()
+    {
     }
 }
 
