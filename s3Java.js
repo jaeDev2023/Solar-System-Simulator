2f5dd249edad2ed46e3ff1c532b9146bbfa81940
jaeDev2023 <jae.dev2023@gmail.com>
2024-01-12 17:24:34 -0500

    added test mesh for camera testing.

--- s3Java.js
+++ s3Java.js
@@ -10,31 +10,27 @@ class AstroObject {
     
     UpdateVelocity (astroBodies, timeStep, gravConstant)
     {
-        // Need to create a global definition for the universal constant.
         for(let i = 0; i < astroBodies.length; i++)
         {
             let otherBody = astroBodies[i];
             if(otherBody != this)
             {
+                // for some reason, the position when called here is undefined. Before the math
+                console.log("Update velocity before math happens, baby");
+                console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
                 let sqrDst = BABYLON.Vector3.DistanceSquared(otherBody.position, this.position);
-                console.log(sqrDst + "----------------");
                 let forceDir = otherBody.position.subtract(this.position).normalize();
                 let force = forceDir.scale(gravConstant * this.mass * otherBody.mass / sqrDst);
                 let acceleration = force.scale(1 / this.mass);
                 this.velocity = this.velocity.add(acceleration.scale(timeStep));
+                console.log("Update velocity values");
+                console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
             }            
         }
     }
 
-    updatePosition (timeStep)
+    UpdatePosition (timeStep)
     {
         this.position = this.velocity.scale(timeStep);
-        console.log(this.position);
-
-    }
-
-    printDebugging ()
-    {
-        //console.log(this.position);
     }
 }
