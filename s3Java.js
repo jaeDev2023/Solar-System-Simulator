5130e86fbebf2afe09e2575f7ab8e6038e20a413
jaeDev2023 <jae.dev2023@gmail.com>
2024-01-28 14:18:38 -0500

    Updated UpdatePosition and UpdateVelocity functions

--- s3Java.js
+++ s3Java.js
@@ -16,21 +16,22 @@ class AstroObject {
             if(otherBody != this)
             {
                 // for some reason, the position when called here is undefined. Before the math
-                console.log("Update velocity before math happens, baby");
-                console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
+                // console.log("Update velocity before math happens, baby");
+                // console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
                 let sqrDst = BABYLON.Vector3.DistanceSquared(otherBody.position, this.position);
                 let forceDir = otherBody.position.subtract(this.position).normalize();
                 let force = forceDir.scale(gravConstant * this.mass * otherBody.mass / sqrDst);
                 let acceleration = force.scale(1 / this.mass);
                 this.velocity = this.velocity.add(acceleration.scale(timeStep));
-                console.log("Update velocity values");
-                console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
+                //this.velocity = this.velocity.add(acceleration.scale(timeStep)); // Very unsure if this is a typo or the proper b.j.s implementation of updating velocity.
+                // console.log("Update velocity values");
+                // console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
             }            
         }
     }
 
     UpdatePosition (timeStep)
     {
-        this.position = this.velocity.scale(timeStep);
+        this.position = this.position.add(this.velocity.scale(timeStep));
     }
 }
