175fe199cd851560a0fc1268d8e53d60beea9f01
jaeDev2023 <jae.dev2023@gmail.com>
2024-03-08 14:46:40 -0500

    Updated physics scripts

--- s3Java.js
+++ s3Java.js
@@ -1,37 +1,44 @@
 class AstroObject {
-    constructor(mass, radius, initialVelocity, initialPosition)
+    constructor(mass, radius, initialVelocity, initialPosition, meshHolder)
     {
         this.mass = mass;
         this.radius = radius;
-        this.velocity = initialVelocity;
+        this.initialVelocity = initialVelocity;
         this.position = initialPosition;
+        this.meshHolder = meshHolder
+        this.mesh = meshHolder;
+        this.velocity = initialVelocity.clone();
         
     }
     
-    UpdateVelocity (astroBodies, timeStep, gravConstant)
+    UpdateVelocity (allBodies, timeStep, gravitationalConstant)
     {
-        for(let i = 0; i < astroBodies.length; i++)
-        {
-            let otherBody = astroBodies[i];
-            if(otherBody != this)
-            {
-                // for some reason, the position when called here is undefined. Before the math
-                // console.log("Update velocity before math happens, baby");
-                // console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
-                let sqrDst = BABYLON.Vector3.DistanceSquared(otherBody.position, this.position);
-                let forceDir = otherBody.position.subtract(this.position).normalize();
-                let force = forceDir.scale(gravConstant * this.mass * otherBody.mass / sqrDst);
-                let acceleration = force.scale(1 / this.mass);
-                this.velocity = this.velocity.add(acceleration.scale(timeStep));
-                //this.velocity = this.velocity.add(acceleration.scale(timeStep)); // Very unsure if this is a typo or the proper b.j.s implementation of updating velocity.
-                // console.log("Update velocity values");
-                // console.log(`Mass:${this.mass} | Radius:${this.radius} | velocity:${this.velocity} | position:${this.position}`);
-            }            
-        }
+        allBodies.forEach(otherBody => {
+            if (otherBody !== this) {
+            const direction = otherBody.position.subtract(this.position);
+            console.log(`Direction: ${direction}`);
+
+            const distanceSquared = direction.lengthSquared();
+            console.log(`distancesquared: ${distanceSquared}`);
+
+            const forceDirection = direction.normalize();
+            console.log(`forceDirection: ${forceDirection}`);
+
+            const forceMagnitude = forceDirection.scale(gravitationalConstant * this.mass * otherBody.mass / distanceSquared);
+            console.log(`forceMagnitude: ${forceMagnitude}`);
+
+            const acceleration = forceMagnitude.scale(1 / this.mass);
+            console.log(`acceleration: ${acceleration}`);
+
+            this.velocity = this.velocity.add(acceleration.scale(timeStep));
+            console.log(`velocity: ${this.velocity}`);
+            }
+        });
     }
 
     UpdatePosition (timeStep)
     {
         this.position = this.position.add(this.velocity.scale(timeStep));
+        console.log(`new position: ${this.position}`);
     }
 }
