008e085f6da817d34bb915515a4b8e3a70ade908
jaeDev2023 <jae.dev2023@gmail.com>
2024-03-10 17:45:38 -0400

    Removed commented out code and changed variables names

--- s3Java.js
+++ s3Java.js
@@ -7,7 +7,7 @@ class AstroObject {
         this.position = initialPosition;
         this.meshHolder = meshHolder
         this.mesh = meshHolder;
-        this.velocity = initialVelocity.clone();
+        this.velocity = initialVelocity;
         
     }
     
@@ -16,29 +16,16 @@ class AstroObject {
         allBodies.forEach(otherBody => {
             if (otherBody !== this) {
             const direction = otherBody.position.subtract(this.position);
-            console.log(`Direction: ${direction}`);
-
             const distanceSquared = direction.lengthSquared();
-            console.log(`distancesquared: ${distanceSquared}`);
-
             const forceDirection = direction.normalize();
-            console.log(`forceDirection: ${forceDirection}`);
-
             const forceMagnitude = forceDirection.scale(gravitationalConstant * this.mass * otherBody.mass / distanceSquared);
-            console.log(`forceMagnitude: ${forceMagnitude}`);
-
             const acceleration = forceMagnitude.scale(1 / this.mass);
-            console.log(`acceleration: ${acceleration}`);
-
-            this.velocity = this.velocity.add(acceleration.scale(timeStep));
-            console.log(`velocity: ${this.velocity}`);
-            }
+            this.velocity = this.velocity.add(acceleration.scale(timeStep));}
         });
     }
 
     UpdatePosition (timeStep)
     {
         this.position = this.position.add(this.velocity.scale(timeStep));
-        console.log(`new position: ${this.position}`);
     }
 }
