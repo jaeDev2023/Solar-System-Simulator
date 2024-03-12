6970133387a70fff1819c4ae72989d9186d8d439
jaeDev2023 <jae.dev2023@gmail.com>
2024-03-11 20:37:20 -0400

    Added skeleton structure for GravityCalculator.js code

---
+++ GravityCalculator.js
@@ -0,0 +1,13 @@
+class GravityCalculator {
+    constructor() {
+
+    }
+    
+    // UpdateObjectPositions(astroBodies, bodyMesh, timestep, gravitaionalConstant) {
+    //     astroBodies.forEach(body => {
+    //         body.UpdateVelocity(astroBodies, timestep, gravitaionalConstant);
+    //         body.UpdatePosition(timestep);
+    //         bodyMesh.position = body.position;
+    //     })
+    // }
+}
