5bca32185c95b4caae059ae2316184e58b8c3bba
jaeDev2023 <jae.dev2023@gmail.com>
2024-07-20 20:10:31 -0400

    test

--- AstroObject.js
+++ AstroObject.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class AstroObject {
     constructor(mass, radius, initialVelocity, initialPosition, meshHolder)
     {
@@ -35,42 +34,4 @@ class AstroObject {
         this.position = this.position.add(this.velocity.scale(timeStep));
         this.meshHolder.position = this.position;
     }
-=======
-class AstroObject {
-    constructor(mass, radius, initialVelocity, initialPosition, meshHolder)
-    {
-        this.mass = mass;
-        this.radius = radius;
-        this.initialVelocity = initialVelocity;
-        this.position = initialPosition;
-        this.meshHolder = meshHolder
-        this.mesh = meshHolder;
-        this.velocity = initialVelocity;
-        
-    }
-    
-    UpdateVelocity (allBodies, timeStep, gravitationalConstant)
-    {
-        allBodies.forEach(otherBody => {
-            if (otherBody !== this) {
-                const direction = otherBody.position.subtract(this.position);
-                
-                const threshold = this.radius + otherBody.radius;
-                const distanceSquared = direction.lengthSquared();
-                if(distanceSquared < threshold) {
-                    console.log("Collision detected. Delete the colliding objects");
-                }                
-                const forceDirection = direction.normalize();
-                const forceMagnitude = forceDirection.scale(gravitationalConstant * this.mass * otherBody.mass / distanceSquared);
-                const acceleration = forceMagnitude.scale(1 / this.mass);
-                this.velocity = this.velocity.add(acceleration.scale(timeStep));}
-        });
-    }
-
-    UpdatePosition (timeStep)
-    {
-        this.position = this.position.add(this.velocity.scale(timeStep));
-        this.meshHolder.position = this.position;
-    }
->>>>>>> 01c5ce9379f26d77738421141d1ddf5dbb5bb654
 }
