5148008361e8c5af8ea51223bc2f17699a0bde1e
jaeDev2023 <jae.dev2023@gmail.com>
2024-09-03 15:48:43 -0400

    adding more content generated from button press

--- script.js
+++ script.js
@@ -14,24 +14,44 @@ document.addEventListener('DOMContentLoaded', function () {
             if (!isActive) {
                 button.classList.add('active');
 
-                let content = '';
+                let contentFragment = document.createDocumentFragment();
+                let content;
                 switch(button.id) {
                     case 'physics-button':
-                        content = '<p>Content for Physics Button</p>';
+                        content = document.createElement('div');
+                        content.innerHTML = `
+                            <h2>The Physics of The Situation: </h2>
+                            <p>Objects of mass affect each other. You can determine the "force" that objects pull each other with the equation below:</p>
+                            <p>F = (G * m<sub>1</sub> * m<sub>2) / r<sup>2</sup></sub></p>
+                            <p>What do these variables mean?</p>
+                            <ul>
+                                <li>F: this is the force that is being calculated.</li>
+                                <li>G: The gravitational constant. A very small value. This is always the same value when using this equation.</li>
+                                <li>m1: the mass of the first object. Increasing this increases the force. Decreasing this value decreases the force.</li>
+                                <li>m2: the mass of the second object. Increase this also increases the force, and decreasing also decreases the resulting force felt between the two objects.</li>
+                                <li>r<sup>2</sup>: the distance between to two objects. Increasing the distance decreases the force between the two objects.</li>
+                            </ul>
+                            <h2>Using Force in the simulation</h2>
+                            <p>The simulation runs in a 3D environment. While the sim runs, we go through every object in the game and use the force equation to calculate the force an individual objects "feels" betweeen all other objects. This will change its velocity and acceleration, which allows the game to update the positions of objects over time.</p>
+                        `;
+                        contentFragment.appendChild(content);
                         break;
                     case 'babylon-button':
-                        content = '<p>Content for Babylon.js button</p>';
+                        content = document.createElement('div');
+                        content.innerHTML = `
+                            <p>Test</p>
+                        `
+                        contentFragment.appendChild(content);
                         break;
                     case 'html-button':
-                        content = '<p>Content for the html button';
+                        content = '<p>Content for the html button</p><hr>';
                         break;
                     case 'css-button':
-                        content = '<p>Content for the css button</p>';
+                        content = '<p>Content for the css button</p><hr>';
                         break
-
                 }
 
-                contentContainer.innerHTML = content;
+                contentContainer.appendChild(contentFragment);
             }
         });
     });
