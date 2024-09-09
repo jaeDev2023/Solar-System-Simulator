2735a8c1ad47218761ab54569079009b3c2e7a2f
jaeDev2023 <jae.dev2023@gmail.com>
2024-09-08 20:38:05 -0400

    Changed style, added pages, and organized the css rules

--- script.js
+++ script.js
@@ -39,15 +39,29 @@ document.addEventListener('DOMContentLoaded', function () {
                     case 'babylon-button':
                         content = document.createElement('div');
                         content.innerHTML = `
-                            <p>Test</p>
+                            <h2>Babylon.js</h2>
+                            <p><a href="https://www.babylonjs.com/">Babylon.js</a> is a powerful 3D engine used to display graphics in browsers!</p>
+                            <p>With it, you can create games, simulations, models, and anything that your heart desires!</p>
+                            <p>S<sup>3</sup> uses the babylon.js API to generate objects within 3D space and uses the power of physics to fling those objects through space!</p>
                         `
                         contentFragment.appendChild(content);
                         break;
                     case 'html-button':
-                        content = '<p>Content for the html button</p><hr>';
+                        content = document.createElement('div');
+                        content.innerHTML = `
+                            <h2>HTML</h2>
+                            <p>HTML is a powerful markup language used to build the framework of a webpage. Place headings, paragraphs, links, images, and establish the basic structure with HyperText Markup Language.</p>
+                        `;
+                        contentFragment.appendChild(content);
                         break;
                     case 'css-button':
-                        content = '<p>Content for the css button</p><hr>';
+                        content = document.createElement('div');
+                        content.innerHTML = `
+                            <h2>CSS</h2>
+                            <p>CSS is the language used to style and set up the presentation of a webpage!</p>
+                            <p>Change color, spacing, font, size, and almost every other visual aspect of a webpage and the elements that make up the webpage. It is an extremely powerful tool for creating the website of your dreams!</p>
+                        `;
+                        contentFragment.appendChild(content);
                         break
                 }
 
